fb59a2f1edd03f8a5911ea20fefff6e0bdf585af
pawelkopec <pawelkopec10@gmail.com>
2017-06-05 11:16:33 +0200

    Improve ServerTree

--- urlcrawler/UrlTree.py
+++ urlcrawler/UrlTree.py
@@ -2,61 +2,63 @@
 class UrlTree(object):
 
     def __init__(self):
-        self.root = {'children': {}}
+        self.root = {'children': []}
 
     def get_node_for_path(self, path):
         nodes = self.path_to_nodes(path)
-        current_leaf = self.root
+        current_node = self.root
 
         for i in range(len(nodes)):
-            if nodes[i] not in current_leaf.get('children'):
+            if not self.has_child_for_name(current_node, nodes[i]):
                 return None
 
-            current_leaf = current_leaf.get('children')[nodes[i]]
+            current_node = self.get_child_by_name(current_node, nodes[i])
 
-        return current_leaf
+        return current_node
 
     def contains_path(self, path):
         return self.get_node_for_path(path) is not None
 
     def has_code_for_path(self, path):
-        leaf = self.get_node_for_path(path)
+        node = self.get_node_for_path(path)
 
-        if leaf is None:
+        if node is None:
             return False
 
-        return leaf.get('response-code') is not None
+        return node.get('response-code') is not None
 
     def add_code_for_path(self, path, code):
-        leaves = self.path_to_nodes(path)
+        nodes = self.path_to_nodes(path)
         current_parent = self.root
 
-        for i in range(len(leaves)):
-            if leaves[i] in current_parent.get('children'):
-                current_parent = current_parent.get('children')[leaves[i]]
+        for i in range(len(nodes)):
+            if self.has_child_for_name(current_parent, nodes[i]):
+                current_parent = self.get_child_by_name(current_parent, nodes[i])
             else:
-                for j in range(i, len(leaves) - 1):
+                for j in range(i, len(nodes) - 1):
                     new_parent = {
-                        'children': {},
-                        'response-code': None
+                        'name': nodes[j],
+                        'response-code': None,
+                        'children': []
                     }
 
-                    current_parent.get('children')[leaves[j]] = new_parent
+                    current_parent.get('children').append(new_parent)
                     current_parent = new_parent
 
                 new_child = {
-                    'children': {},
-                    'response-code': code
+                    'name': nodes[-1],
+                    'response-code': code,
+                    'children': []
                 }
 
-                current_parent.get('children')[leaves[-1]] = new_child
+                current_parent.get('children').append(new_child)
 
                 return
 
         current_parent['response-code'] = code
 
     def as_dict(self):
-        hidden_root = self.root.get('children').get('')
+        hidden_root = self.get_child_by_name(self.root, self.ROOT_DUMMY)
 
         if hidden_root:
             tree = hidden_root.copy()
@@ -70,12 +72,23 @@ class UrlTree(object):
         if not node.get('response-code'):
             node.pop('response-code', None)
 
-        for child in node.get('children').values():
+        for child in node.get('children'):
             self.remove_none_keys(child)
 
         if len(node.get('children')) == 0:
             node.pop('children', None)
 
-    @staticmethod
-    def path_to_nodes(path):
-        return [''] + path.split('/')[1:]
+    def get_child_by_name(self, node, name):
+        for child in node.get('children'):
+            if child.get('name') == name:
+                return child
+
+        return None
+
+    def has_child_for_name(self, node, name):
+        return self.get_child_by_name(node, name) is not None
+
+    def path_to_nodes(self, path):
+        return [self.ROOT_DUMMY] + path.split('/')[1:]
+
+    ROOT_DUMMY = ''
