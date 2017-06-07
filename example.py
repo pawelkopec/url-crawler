9469d224d7334278e8d1cc34d27d1b6d12283026
pawelkopec <pawelkopec10@gmail.com>
2017-06-08 01:17:33 +0200

    Fix link in example

--- example.py
+++ example.py
@@ -5,6 +5,6 @@ from urlcrawler.Crawler import Crawler
 crawler = Crawler()
 
 google_tree = crawler.search_active_urls(
-    "http://fanjet.pl", logs=True,)
+    "http://google.com", logs=True,)
 
 print(json.dumps(google_tree, sort_keys=True, indent=2))
