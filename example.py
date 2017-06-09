9a0db4048deae93e5f94a691c595482a0cd4e6d1
pawelkopec <pawelkopec10@gmail.com>
2017-06-09 15:13:17 +0200

    Remove unnecessary ","

--- example.py
+++ example.py
@@ -5,6 +5,6 @@ from urlcrawler.Crawler import Crawler
 crawler = Crawler()
 
 google_tree = crawler.get_website_tree(
-    "http://google.com", logs=True,)
+    "http://google.com", logs=True)
 
 print(json.dumps(google_tree, sort_keys=True, indent=2))
